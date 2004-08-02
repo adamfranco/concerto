3cad9873fa9181643dc1932f8d5f21d62b0a1314
Adam Franco <afranco@middlebury.edu>
2004-08-02 17:50:11 +0000

    Schema creation should now work. There currently seem to be problems with the DR/datamanager that are preventing saving, but the application parts should be good.

--- main/modules/schema/create.act.php
+++ main/modules/schema/create.act.php
@@ -27,14 +27,14 @@ $centerPane =& $harmoni->getAttachedData('centerPane');
 	
 	// Create the properties.
 	$displayNameProp =& $stepOne->createProperty("schema_display_name", new RegexValidatorRule("^[^ ]{1}.*$"));
-	$displayNameProp->setDefaultValue("Default Schema Name");
+	$displayNameProp->setDefaultValue("");
 	$displayNameProp->setErrorString(" <span style='color: f00'>* "._("The name must not start with a space.")."</span>");
-
-	$formatProp =& $stepOne->createProperty("schema_format", new RegexValidatorRule(".*"));
-	$formatProp->setDefaultValue("Plain Text - UTF-8 encoding");
 	
 	$descriptionProp =& $stepOne->createProperty("schema_description", new RegexValidatorRule(".*"));
-	$descriptionProp->setDefaultValue("Default Schema description.");
+	$descriptionProp->setDefaultValue("");
+	
+	$formatProp =& $stepOne->createProperty("format", new RegexValidatorRule(".*"));
+	$formatProp->setDefaultValue("Plain Text - UTF-8 encoding");
 	
 	
 	// Create the step text
@@ -48,14 +48,14 @@ $centerPane =& $harmoni->getAttachedData('centerPane');
 	print "\n<h2>"._("Format")."</h2>";
 	print "\n"._("The format of data that is entered into the fields: ");
 	print "\n<br /><em>"._("'Plain Text - ASCII encoding', 'XML', etc.")."</em>";
-	print "\n<br><input type='text' name='schema_format' value=\"[[schema_format]]\" size='25'>[[schema_format|Error]]";
+	print "\n<br><input type='text' name='format' value=\"[[format]]\" size='25'>[[format|Error]]";
 	print "\n<div style='width: 400px'> &nbsp; </div>";
 	$stepOne->setText(ob_get_contents());
 	ob_end_clean();
 		
 	
 	// :: Add Elements ::
-	$elementStep =& $wizard->addStep(new MultiValuedWizardStep(_("Add Elements")));
+	$elementStep =& $wizard->addStep(new MultiValuedWizardStep(_("Add Elements"), "elements"));
 	$_SESSION['create_schema_wizard_'.$harmoni->pathInfoParts[2]."element_step"] =& $elementStep;
 	
 	$property =& $elementStep->createProperty("display_name", new RegexValidatorRule("^[^ ]{1}.*$"));
@@ -85,17 +85,17 @@ $centerPane =& $harmoni->getAttachedData('centerPane');
 	
 	print "\n<table border=\"0\">";
 		print "\n<tr><td>";
-			print "DisplayName: ";
+			print _("DisplayName").": ";
 		print "\n</td><td>";
 			print "<input type='text' name='display_name' value=\"[[display_name]]\">[[display_name|Error]]";
 		print "\n</td></tr>";
 		print "\n<tr><td>";
-			print "Description: ";
+			print _("Description").": ";
 		print "\n</td><td>";
 			print "<textarea name=\"description\" rows=\"3\" cols=\"25\">[[description]]</textarea>[[description|Error]]";
 		print "\n</td></tr>";
 		print "\n<tr><td>";
-			print "Select a Type... ";
+			print _("Select a Type")."... ";
 		print "\n</td><td>";
 			print "\n<select name=\"type\">";
 			// We are going to assume that all InfoStructures have the same InfoPartTypes
@@ -119,21 +119,21 @@ $centerPane =& $harmoni->getAttachedData('centerPane');
 		print "\n</td></tr>";
 
 		print "\n<tr><td>";
-			print "isMandatory? ";
+			print _("isMandatory? ");
 		print "\n</td><td>";
 			print "<input type=\"radio\" name='mandatory' value='TRUE' [['mandatory'=='TRUE'| checked|]] />TRUE / ";
 			print "<input type=\"radio\" name='mandatory' value='FALSE' [['mandatory'=='FALSE'| checked|]] /> FALSE";
 		print "\n</td></tr>";
 		
 		print "\n<tr><td>";
-			print "isRepeatable? ";
+			print _("isRepeatable? ");
 		print "\n</td><td>";
 			print "<input type=\"radio\" name='repeatable' value='TRUE' [['repeatable'=='TRUE'| checked|]] />TRUE / ";
 			print "<input type=\"radio\" name='repeatable' value='FALSE' [['repeatable'=='FALSE'| checked|]] /> FALSE";
 		print "\n</td></tr>";
 		
 		print "\n<tr><td>";
-			print "isPopulatedByDR? ";
+			print _("isPopulatedByDR? ");
 		print "\n</td><td>";
 			print "<input type=\"radio\" name='populatedbydr' value='TRUE' [['populatedbydr'=='TRUE'| checked|]] />TRUE / ";
 			print "<input type=\"radio\" name='populatedbydr' value='FALSE' [['populatedbydr'=='FALSE'| checked|]] /> FALSE";
@@ -143,23 +143,22 @@ $centerPane =& $harmoni->getAttachedData('centerPane');
 	
 	print "\n<br />[Buttons]";
 	print "\n<hr>";
-	print "Elements Added:";
-	print "\n<table>[List]<tr><td valign='top'>[ListButtons]<br />[ListMoveButtons]</td>";
-	print "<td style='padding-bottom: 20px'>";
-	print "<strong>DisplayName:</strong> [[display_name]]";
-	print "<br /><strong>Description:</strong> [[description]]";
-	print "<br /><strong>Type:</strong> [[type]]";
-	print "<br /><strong>isMandatory:</strong> [[mandatory]]";
-	print "<br /><strong>isRepeatable:</strong> [[repeatable]]";
-	print "<br /><strong>isPopulatedByDR:</strong> [[populatedbydr]]";
-	print "</td></tr>[/List]</table>";
+	print _("Elements Added:");
+	print "\n<table>";
+	print "[List]\n<tr>";
+	print "\n<td valign='top'>[ListButtons]<br />[ListMoveButtons]</td>";
+	print "\n<td style='padding-bottom: 20px'>";
+	print "\n\t<strong>"._("DisplayName").":</strong> [[display_name]]";
+	print "\n\t<br /><strong>"._("Description").":</strong> [[description]]";
+	print "\n\t<br /><strong>"._("Type").":</strong> [[type]]";
+	print "\n\t<br /><strong>"._("isMandatory").":</strong> [[mandatory]]";
+	print "\n\t<br /><strong>"._("isRepeatable").":</strong> [[repeatable]]";
+	print "\n\t<br /><strong>"._("isPopulatedByDR").":</strong> [[populatedbydr]]";
+	print "</td>\n</tr>[/List]\n</table>";
 
 	$elementStep->setText(ob_get_contents());
 	ob_end_clean();
 }
-
-// If we've added a new element, add it to the list.
-
 
 // Prepare the return URL so that we can get back to where we were.
 $currentPathInfo = array();
@@ -168,24 +167,40 @@ for ($i = 3; $i < count($harmoni->pathInfoParts); $i++) {
 }
 $returnURL = MYURL."/".implode("/",$currentPathInfo);
 
-if ($wizard->isSaveRequested() || $_REQUEST['create_schema']) {
+if ($wizard->isSaveRequested()) {
 	// If all properties validate then go through the steps nessisary to
 	// save the data.
 	if ($wizard->updateLastStep()) {
 		$properties =& $wizard->getProperties();
 		
-		// Save the DR
+		// Get the DR
 		$shared =& Services::getService("Shared");
 		$id =& $shared->getId($harmoni->pathInfoParts[2]);
 		
 		$drManager =& Services::getService("DR"); 
 		$dr =& $drManager->getDigitalRepository($id);
 		
-		$dr->updateDisplayName($properties['display_name']->getValue());
-		$dr->updateDescription($properties['description']->getValue());
-		
-		// Save the Schema settings.
-		// @todo
+		// Create the info Structure
+		$infoStructure =& $dr->createInfoStructure($properties['schema_display_name']->getValue(), 
+								$properties['schema_description']->getValue(), 
+								$properties['format']->getValue(),
+								$properties['schema_display_name']->getValue());
+								
+		// Create the infoParts
+		$infoPartProperties =& $properties['elements'];
+		foreach (array_keys($infoPartProperties) as $index) {
+			$typeString = urldecode($infoPartProperties[$index]['type']->getValue());
+			$typeParts = explode("/", $typeString);
+			$type =& new HarmoniType($typeParts[0], $typeParts[1], $typeParts[2], $typeParts[3]);
+			$infoStructure->createInfoPart(
+							$infoPartProperties[$index]['display_name']->getValue(),
+							$infoPartProperties[$index]['description']->getValue(),
+							$type,
+							(($infoPartProperties[$index]['mandatory']->getValue())?TRUE:FALSE),
+							(($infoPartProperties[$index]['repeatable']->getValue())?TRUE:FALSE),
+							(($infoPartProperties[$index]['populatedbydr']->getValue())?TRUE:FALSE)
+							);
+		}
 		
 		// Unset the wizard
 		$wizard = NULL;
