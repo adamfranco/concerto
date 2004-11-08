22d104d57481b833295b6fbae2b1d92b0ea07def
Adam Franco <afranco@middlebury.edu>
2004-11-08 15:48:23 +0000

    Replacing with output buffers.

--- main/modules/collections/main.act.php
+++ main/modules/collections/main.act.php
@@ -18,31 +18,33 @@ $introHeader->addComponent(new Content(_("Collections")));
 $actionRows->addComponent($introHeader);
 
 $introText =& new SingleContentLayout(TEXT_BLOCK_WIDGET, 2);
-$text = "";
-//$text .= "\n<img src='".MYPATH."/main/modules/home/flower.jpg' alt='A flower. &copy;2003 Adam Franco - Creative Commons Attribution-ShareAlike 1.0 - http://creativecommons.org/licenses/by-sa/1.0/' align='right' style='margin: 10px;' />";
-$text .= "<p>";
-$text .= _("<em>Collections</em> are containers for <em>Assets</em>. <em>Assets</em> can in turn contain other Assets. Each collection can have its own cataloging schema.");
-$text .= "</p>\n<ul>";
-$text .= "\n\t<li><a href='".MYURL."/collections/namebrowse/'>";
-$text .= _("Browse <em>Collections</em> by Name");
-$text .= "</a></li>";
-$text .= "\n\t<li><a href='".MYURL."/collections/typebrowse/'>";
-$text .= _("Browse <em>Collections</em> by Type");
-$text .= "</a></li>";
-$text .= "\n\t<li><a href='".MYURL."/collections/search/'>";
-$text .= _("Search <em>Collections</em> for <em> Assets</em>");
-$text .= "</a></li>";
-$text .= "</ul>\n<p>";
-$text .= _("Some <em>Collections</em>, <em>Exhibitions</em>, <em>Assets</em>, and <em>Slide-Shows</em> may be restricted to certain users or groups of users. Log in above to ensure your greatest access to all parts of the system.");
-$text .= "</p>";
+ob_start();
+print "";
+//print "\n<img src='".MYPATH."/main/modules/home/flower.jpg' alt='A flower. &copy;2003 Adam Franco - Creative Commons Attribution-ShareAlike 1.0 - http://creativecommons.org/licenses/by-sa/1.0/' align='right' style='margin: 10px;' />";
+print "<p>";
+print _("<em>Collections</em> are containers for <em>Assets</em>. <em>Assets</em> can in turn contain other Assets. Each collection can have its own cataloging schema.");
+print "</p>\n<ul>";
+print "\n\t<li><a href='".MYURL."/collections/namebrowse/'>";
+print _("Browse <em>Collections</em> by Name");
+print "</a></li>";
+print "\n\t<li><a href='".MYURL."/collections/typebrowse/'>";
+print _("Browse <em>Collections</em> by Type");
+print "</a></li>";
+print "\n\t<li><a href='".MYURL."/collections/search/'>";
+print _("Search <em>Collections</em> for <em> Assets</em>");
+print "</a></li>";
+print "</ul>\n<p>";
+print _("Some <em>Collections</em>, <em>Exhibitions</em>, <em>Assets</em>, and <em>Slide-Shows</em> may be restricted to certain users or groups of users. Log in above to ensure your greatest access to all parts of the system.");
+print "</p>";
 
 // If the user is authorized, allow them to create a new collection.
 // @todo - add authorization.
-$text .= "\n<ul>\n<li><a href='".MYURL."/collection/create/'>";
-$text .= _("Create a new <em>Collection</em>");
-$text .= "</a>\n</li>\n</ul>";
+print "\n<ul>\n<li><a href='".MYURL."/collection/create/'>";
+print _("Create a new <em>Collection</em>");
+print "</a>\n</li>\n</ul>";
 
-$introText->addComponent(new Content($text));
+$introText->addComponent(new Content(ob_get_contents()));
+ob_end_clean();
 $actionRows->addComponent($introText);
 
 // return the main layout.
