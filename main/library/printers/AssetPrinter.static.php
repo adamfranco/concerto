ca62c0e1c3eca0bc5361525e6b511fb123678048
Mihai Azoitei <mazoitei@middlebury.edu>
2005-03-24 22:44:08 +0000

    printer functions for exhibition added

--- main/library/printers/AssetPrinter.static.php
+++ main/library/printers/AssetPrinter.static.php
@@ -27,6 +27,7 @@ class AssetPrinter {
 	 * @access public
 	 * @date 8/6/04
 	 */
+	 
 	function printAssetFunctionLinks (& $harmoni, & $asset, $repositoryId = NULL) {
 		if (!defined("AZ_ACCESS"))
 			throwError(new Error("You must define an id for AZ_ACCESS", "concerto.collection", true));
@@ -39,6 +40,7 @@ class AssetPrinter {
 		if (!defined("AZ_ADD_CHILDREN"))
 			throwError(new Error("You must define an id for AZ_ADD_CHILDREN", "concerto.collection", true));
 
+    $slideshowId =& new HarmoniId("529");
 		$authZ =& Services::getService("AuthZ");
 		$idManager =& Services::getService("Id");
 		
@@ -88,6 +90,23 @@ class AssetPrinter {
 				$links[] = _("edit");
 			}
 		}
+		if(true){
+		//if(!$slideshowId->isEqual("0"){
+     // if ($authZ->isUserAuthorized($idManager->getId(AZ_EDIT), $asset->getId())) {
+	      		if ($actionString != "asset.add-to-slideshow") {
+		      		$links[] = "<a href='".MYURL."/'>";
+              $repositoryManager =& Services::getService("Repository");
+              $idManager =& Services::getService("Id");
+              $repositoryId =& $idManager->getId($harmoni->pathInfoParts[2]);
+              $repository =& $repositoryManager->getRepository($repositoryId);
+              $repository->_copyAsset($asset, $slideshowId);
+           
+				      $links[count($links) - 1] .= _("add to slideshow")."</a>";
+			      } else {
+				      $links[] = _("add-to-slideshow");
+			      }
+		 // }
+    }
 		
 		if ($authZ->isUserAuthorized($idManager->getId(AZ_DELETE), $asset->getId())) {
 			if ($actionString != "asset.delete") {
@@ -127,7 +146,100 @@ class AssetPrinter {
 		print  implode("\n\t | ", $links);
 	}
 	
-	
+	function printAssetFunctionLinksExh (& $harmoni, & $asset, $repositoryId = NULL) {
+		if (!defined("AZ_ACCESS"))
+			throwError(new Error("You must define an id for AZ_ACCESS", "concerto.exhibition", true));
+		if (!defined("AZ_VIEW"))
+			throwError(new Error("You must define an id for AZ_VIEW", "concerto.exhibition", true));
+		if (!defined("AZ_EDIT"))
+			throwError(new Error("You must define an id for AZ_EDIT", "concerto.exhibition", true));
+		if (!defined("AZ_DELETE"))
+			throwError(new Error("You must define an id for AZ_DELETE", "concerto.exhibition", true));
+		if (!defined("AZ_ADD_CHILDREN"))
+			throwError(new Error("You must define an id for AZ_ADD_CHILDREN", "concerto.exhibition", true));
+
+		$authZ =& Services::getService("AuthZ");
+		$idManager =& Services::getService("Id");
+
+		$assetId =& $asset->getId();
+		if ($repositoryId === NULL) {
+			$repository =& $asset->getRepository();
+			$repositoryId =& $repository->getId();
+		}
+
+		$links = array();
+
+		$actionString = $harmoni->getCurrentAction();
+
+		if ($authZ->isUserAuthorized($idManager->getId(AZ_VIEW), $asset->getId())) {
+			if ($actionString != "asset.view") {
+				$links[] = "<a href='".MYURL."/asset/view/".$repositoryId->getIdString()."/".$assetId->getIdString()."/'>";
+				$links[count($links) - 1] .= _("view")."</a>";
+			} else {
+				$links[] = _("view");
+			}
+		}
+
+		if ($authZ->isUserAuthorized($idManager->getId(AZ_ACCESS), $asset->getId())) {
+			$children =& $asset->getAssets();
+			if ($children->hasNext()) {
+				if ($actionString != "asset.browse" || $assetId->getIdString() != $harmoni->pathInfoParts[3]) {
+					$links[] = "<a href='".MYURL."/asset/browse/".$repositoryId->getIdString()."/".$assetId->getIdString()."/'>";
+					$links[count($links) - 1] .= _("browse")."</a>";
+				} else {
+					$links[] = _("browse");
+				}
+
+			}
+		}
+
+		if ($authZ->isUserAuthorized($idManager->getId(AZ_EDIT), $asset->getId())) {
+			if ($actionString != "asset.editview") {
+				$links[] = "<a href='".MYURL."/asset/editview/".$repositoryId->getIdString()."/".$assetId->getIdString()."/'>";
+				$links[count($links) - 1] .= _("edit")."</a>";
+			} else {
+				$links[] = _("edit");
+			}
+		}
+
+		if ($authZ->isUserAuthorized($idManager->getId(AZ_DELETE), $asset->getId())) {
+			if ($actionString != "asset.delete") {
+				ob_start();
+				print "<a href='Javascript:deleteAsset".$assetId->getIdString()."From".$repositoryId->getIdString()."();'";
+				print ">";
+				print _("delete")."</a>";
+
+				print "\n<script type='text/javascript'>\n//<![CDATA[";
+				print "\n	function deleteAsset".$assetId->getIdString()."From".$repositoryId->getIdString()."() {";
+				print "\n	var url;";
+				print "\n		url = '".MYURL."/asset/delete/".$repositoryId->getIdString()."/".$assetId->getIdString()."/";
+				if (ereg("^asset\..*$", $actionString))
+					print "exhibition/browse/".$repositoryId->getIdString()."/';";
+				else
+					print implode("/", $harmoni->pathInfoParts)."/';";
+				print "\n		if (confirm(\""._("Are you sure you want to delete this Asset?")."\")) {";
+				print "\n			window.location = url;";
+				print "\n		}";
+				print "\n	}";
+				print "\n//]]>\n</script>";
+
+				$links[] = ob_get_contents();
+				ob_end_clean();
+			} else {
+				$links[] = _("delete");
+			}
+		}
+
+		if ($authZ->isUserAuthorized($idManager->getId(AZ_ADD_CHILDREN), $asset->getId())) {
+			if (ereg("^asset\..*$", $actionString) && $harmoni->pathInfoParts[3] == $assetId->getIdString()) {
+				$links[] = "<a href='".MYURL."/asset/addchild/".$repositoryId->getIdString()."/".$assetId->getIdString()."/'>";
+				$links[count($links) - 1] .= _("add child asset")."</a>";
+			}
+		}
+
+		print  implode("\n\t | ", $links);
+	}
+
 	
 }
 
--- main/library/printers/RepositoryPrinter.static.php
+++ main/library/printers/RepositoryPrinter.static.php
@@ -17,16 +17,94 @@ class RepositoryPrinter {
 	function RepositoryPrinter () {
 		die("Static class RepositoryPrinter can not be instantiated.");
 	}
-	
-	/**
+
+ /**
 	 * Print links for the various functions that are possible to do with this
 	 * Repository.
-	 * 
+	 *
 	 * @param object Repository $repository The Repository to print the links for.
 	 * @return void
 	 * @access public
 	 * @date 8/6/04
 	 */
+
+   function printRepositoryFunctionLinksExh (& $harmoni, & $repository) {
+		if (!defined("AZ_ACCESS"))
+			throwError(new Error("You must define an id for AZ_ACCESS", "concerto.exhibition", true));
+		if (!defined("AZ_VIEW"))
+			throwError(new Error("You must define an id for AZ_VIEW", "concerto.exhibition", true));
+		if (!defined("AZ_EDIT"))
+			throwError(new Error("You must define an id for AZ_EDIT", "concerto.exhibition", true));
+		if (!defined("AZ_DELETE"))
+			throwError(new Error("You must define an id for AZ_DELETE", "concerto.exhibition", true));
+		if (!defined("AZ_ADD_CHILDREN"))
+			throwError(new Error("You must define an id for AZ_ADD_CHILDREN", "concerto.exhibition", true));
+
+		$authZ =& Services::getService("AuthZ");
+		$idManager =& Services::getService("Id");
+		$repositoryId =& $repository->getId();
+
+		$links = array();
+
+		$actionString = $harmoni->getCurrentAction();
+
+		if ($authZ->isUserAuthorized($idManager->getId(AZ_ACCESS), $repositoryId)) {
+			if ($actionString != "exhibition.browse") {
+				$links[] = "<a href='".MYURL."/exhibition/browse/".$repositoryId->getIdString()."/'>";
+				$links[count($links) - 1] .= _("browse")."</a>";
+			} else {
+				$links[] = _("browse");
+			}
+
+			if ($actionString != "exhibition.search") {
+				$links[] = "<a href='".MYURL."/exhibition/search/".$repositoryId->getIdString()."/'>";
+				$links[count($links) - 1] .= _("search")."</a>";
+			} else {
+				$links[] = _("search");
+			}
+    }
+
+		if ($authZ->isUserAuthorized($idManager->getId(AZ_EDIT), $repositoryId)) {
+			$links[] = "<a href='".MYURL."/exhibition/edit/".$repositoryId->getIdString()."/'>";
+			$links[count($links) - 1] .= _("edit")."</a>";
+		}
+
+   	if ($authZ->isUserAuthorized($idManager->getId(AZ_DELETE), $repositoryId)) {
+			if ($actionString != "exhibition.delete") {
+				ob_start();
+				print "<a href='Javascript:deleteExhibition".$repositoryId."();'";
+				print ">";
+				print _("delete")."</a>";
+
+				print "\n<script type='text/javascript'>\n//<![CDATA[";
+				print "\n	function deleteExhibition".$repositoryId."() {";
+				print "\n	var url;";
+				print "\n		url = '".MYURL."/exhibition/delete/".$repositoryId->getIdString()."/";
+				if (ereg("^exhibition\..*$", $actionString))
+					print "exhibition/browse/".$repositoryId->getIdString()."/';";
+				else
+					print implode("/", $harmoni->pathInfoParts)."/';";
+				print "\n		if (confirm(\""._("Are you sure you want to delete this Exhibition?")."\")) {";
+				print "\n			window.location = url;";
+				print "\n		}";
+				print "\n	}";
+				print "\n//]]>\n</script>";
+
+				$links[] = ob_get_contents();
+				ob_end_clean();
+			} else {
+				$links[] = _("delete");
+			}
+		}
+
+	 	if ($authZ->isUserAuthorized($idManager->getId(AZ_ADD_CHILDREN), $repositoryId)) {
+			$links[] = "<a href='".MYURL."/asset/add/".$repositoryId->getIdString()."/".implode("/",$harmoni->pathInfoParts)."'>";
+			$links[count($links) - 1] .= _("add asset")."</a>";
+		}
+
+		print  implode("\n\t | ", $links);
+	}
+
 	function printRepositoryFunctionLinks (& $harmoni, & $repository) {
 		if (!defined("AZ_ACCESS"))
 			throwError(new Error("You must define an id for AZ_ACCESS", "concerto.collection", true));
