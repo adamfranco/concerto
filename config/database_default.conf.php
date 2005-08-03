34b3edd89ac5f49437689ee2b36e1634e29c81ae
Adam Franco <afranco@middlebury.edu>
2005-08-03 21:08:01 +0000

    changing default name

--- config/database_default.conf.php
+++ config/database_default.conf.php
@@ -18,7 +18,7 @@
 	
 	//Set up the database connection
 	$databaseManager =& Services::getService("DatabaseManager");
-	$dbName = "afranco_concerto";
+	$dbName = "my_concerto_database";
 	$dbID = $databaseManager->addDatabase( new MySQLDatabase("localhost", $dbName,"test","test") );
 	$databaseManager->pConnect($dbID);
 	unset($databaseManager); // done with that for now
