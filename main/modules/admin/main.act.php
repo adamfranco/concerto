4f0375c2e8cd00cf032056e02160d83c879c8b13
Christopher W. Shubert <cshubert@middlebury.edu>
2005-09-27 17:39:58 +0000

    added import link

--- main/modules/admin/main.act.php
+++ main/modules/admin/main.act.php
@@ -105,7 +105,13 @@ class mainAction
 		print "\n\t<li><a href='".$harmoni->request->quickURL("admin","main", array('reset_concerto' => 'TRUE'))."'>";
 		print _("Reset Concerto");
 		print "</a></li>";
+		print "\n\t<li><a href='".$harmoni->request->quickURL("admin", 
+			"import")."'>";
+		print _("Import");
+		print "</a></li>";
 		print "\n</ul>";
+		
+		
 		
 		$introText =& new Block(ob_get_contents(),3);
 		$actionRows->add($introText, "100%", null, CENTER, CENTER);
