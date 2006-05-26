2b439f78f877c37f6a9a7410c914241a6a7b5a53
Christopher W. Shubert <cshubert@middlebury.edu>
2006-05-26 13:53:37 +0000

    adding mediaDB migration scripts to cvs

---
+++ main/modules/admin/authtrans.act.php
@@ -0,0 +1,349 @@
+<?php
+
+require_once(POLYPHONY."/main/library/AbstractActions/MainWindowAction.class.php");
+
+// apd_set_pprof_trace(); 
+
+class authtransAction 
+	extends MainWindowAction {
+	/**
+	 * Check Authorizations
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function isAuthorizedToExecute () {
+		return TRUE;
+	}
+	
+	/**
+	 * Return the heading text for this action, or an empty string.
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function getHeadingText () {
+		return _("Authorization Transfer");
+	}
+
+	/**
+	 * Build the content for this action
+	 * 
+	 * @return void
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function buildContent () {	
+		$dbHandler =& Services::getService("DBHandler");
+		$rm =& Services::getService("Repository");
+		$agentM =& Services::getService("Agent");
+		$idManager =& Services::getService("Id");
+
+
+print "lines 46 - 49 in authtrans.act.php need to be modified for database access";
+//		$mdbIndex = $dbHandler->addDatabase(
+//			new MySQLDatabase("host", "db", "uname", "password"));
+//		$dbHandler->connect($mdbIndex);
+exit();
+
+		$searchTypes =& $agentM->getAgentSearchTypes();
+		$searchType =& $searchTypes->next();
+		
+		$centerPane =& $this->getActionRows();
+		ob_start();
+		
+		$unamesQuery =& new SelectQuery;
+		$unamesQuery->addTable("users");
+		$unamesQuery->addColumn("uname");
+		$unamesQuery->addColumn("email");
+		$unamesQuery->addOrderBy("uname");
+		
+		$unamesResults =& $dbHandler->query($unamesQuery, $mdbIndex);
+
+		$unameToId = array(); // key on mdb uname value to c_beta id
+
+		while ($unamesResults->hasMoreRows()) {
+			$unamePair = $unamesResults->next();
+			
+			if (!is_null($unamePair['email'])) {
+				$agents =& $agentM->getAgentsBySearch($unamePair['email'],
+					$searchType);
+				if ($agents->hasNext())
+					$agent =& $agents->next();
+				$unameToId[$unamePair['uname']] =& $agent->getId();
+			}
+		}
+	// ===== at this point we have unames and ids
+	
+		$mediasetsQuery =& new SelectQuery;
+		$mediasetsQuery->addTable("mediasets");
+		$mediasetsQuery->addColumn("title");
+		$mediasetsQuery->addColumn("editors");
+		$mediasetsQuery->addColumn("presenters");
+		$mediasetsQuery->addColumn("search");
+		$mediasetsQuery->addOrderBy("title");
+		
+		$mediasetsResults =& $dbHandler->query($mediasetsQuery, $mdbIndex);
+		
+		$repositories =& $rm->getRepositories();
+		$reps = array();
+		while ($repositories->hasNext()) {
+			$rep =& $repositories->next();
+			
+			$reps[$rep->getDisplayName()] = array('id' => $rep->getId());
+		}
+		unset($reps['All Exhibitions']);
+	// ===== at this point we have repository id's associated with displaynames
+	
+		while ($mediasetsResults->hasMoreRows()) {
+			$mediasetInfo = $mediasetsResults->next();
+			if (isset($reps[$mediasetInfo['title']])) {
+				$editors = unserialize($mediasetInfo['editors']);
+				$presenters = unserialize($mediasetInfo['presenters']);
+
+				if (count($editors) > 0) {
+					$reps[$mediasetInfo['title']]['editors'] = array();
+				}
+				foreach ($editors as $editor) {
+					if (isset($unameToId[trim($editor)]))
+						$reps[$mediasetInfo['title']]['editors'][] =
+							$unameToId[trim($editor)];
+				}
+				if (count($presenters) > 0)
+					$reps[$mediasetInfo['title']]['presenters'] = array();
+				foreach ($presenters as $presenter) {
+					if (isset($unameToId[trim($presenter)]))
+						$reps[$mediasetInfo['title']]['presenters'][] =
+							$unameToId[trim($presenter)];
+				}
+				switch ($mediasetInfo['search']) {
+					case '1':
+						$reps[$mediasetInfo['title']]['search'] = 'edu.middlebury.agents.everyone';
+						break;
+					case '2':
+						$reps[$mediasetInfo['title']]['search'] = 'edu.middlebury.agents.users';
+						break;
+					case '3':
+						$reps[$mediasetInfo['title']]['search'] = 'CN=All Faculty,OU=General,OU=Groups,DC=middlebury,DC=edu';
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	// ===== at this point reps has presenters and editors for each mediaset
+
+		$pressetsQuery =& new SelectQuery;
+		$pressetsQuery->addTable("pressets");
+		$pressetsQuery->addColumn("title");
+		$pressetsQuery->addColumn("presenters");
+		$pressetsQuery->addColumn("owner");
+		$pressetsQuery->addColumn("view");
+		$pressetsQuery->addOrderBy("title");
+		
+		$pressetsResults =& $dbHandler->query($pressetsQuery, $mdbIndex);
+
+		$erep =& $rm->getRepository(
+			$idManager->getId("edu.middlebury.concerto.exhibition_repository"));
+
+		$exhibitions =& $erep->getAssets();
+		$exhibits = array();
+		while ($exhibitions->hasNext()) {
+			$exhibit =& $exhibitions->next();
+			
+			$exhibits[$exhibit->getDisplayName()] = array('id' => $exhibit->getId());
+		}
+	// ===== at this point we have exhibition id's associated with display names
+		
+		while ($pressetsResults->hasMoreRows()) {
+			$pressetInfo = $pressetsResults->next();
+			if (isset($exhibits[$pressetInfo['title']])) {
+				$presenters = unserialize($pressetInfo['presenters']);
+
+				if (!is_null($pressetInfo['owner'])) {
+					$owners =& $agentM->getAgentsBySearch(
+						$pressetInfo['owner'], $searchType);
+					if ($owners->count() == 1) {
+						$owner =& $owners->next();
+						$exhibits[$pressetInfo['title']]['owner'] = 
+							$owner->getId();
+					}
+				}
+				if (count($presenters) > 0)
+					$exhibits[$pressetInfo['title']]['presenters'] = array();
+				foreach ($presenters as $presenter) {
+					if (isset($unameToId[trim($presenter)]))
+						$exhibits[$pressetInfo['title']]['presenters'][] =
+							$unameToId[trim($presenter)];
+				}
+				switch ($pressetInfo['view']) {
+					case '1':
+						$reps[$pressetInfo['title']]['view'] = 'everyone';
+						break;
+					case '2':
+						$reps[$pressetInfo['title']]['view'] = 'users';
+						break;
+					case '3':
+						$reps[$mediasetInfo['title']]['search'] = 'CN=All Faculty,OU=General,OU=Groups,DC=middlebury,DC=edu';
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		
+	// ===== at this point all the necessary authz's should be queued up
+		
+		$uIdToAuths = array(); // key on c_beta uId's value to assoc array
+
+// ===== GIVE THE AUTHORIZATIONS
+		$authZ =& Services::getService("AuthZ");
+
+		$viewFunctions = array(
+			$idManager->getId('edu.middlebury.authorization.access'),
+			$idManager->getId('edu.middlebury.authorization.view'),
+			$idManager->getId('edu.middlebury.authorization.comment'));
+
+		$editFunctions = array(
+			$idManager->getId('edu.middlebury.authorization.modify'),
+			$idManager->getId('edu.middlebury.authorization.delete'),
+			$idManager->getId('edu.middlebury.authorization.add_children'),
+			$idManager->getId('edu.middlebury.authorization.remove_children'));
+			
+		$adminFunctions = array(
+	$idManager->getId('edu.middlebury.authorization.view_authorizations'),
+	$idManager->getId('edu.middlebury.authorization.modify_authorizations'));
+
+
+		foreach ($reps as $repAuths) {
+			$repId =& $repAuths['id'];
+			if (isset($repAuths['editors'])) {
+				foreach ($repAuths['editors'] as $editorId) {
+					foreach ($viewFunctions as $vfnId) {
+						$this->addAuth($editorId, $vfnId, $repId, $uIdToAuths);
+// 					$authZ->createAuthorization(
+// 						$editorId,
+// 						$vfnId,
+// 						$repId);
+						}
+					foreach ($editFunctions as $efnId) {
+						$this->addAuth($editorId, $efnId, $repId, $uIdToAuths);
+// 					$authZ->createAuthorization(
+// 						$editorId,
+// 						$efnId,
+// 						$repId);
+					}
+					foreach ($adminFunctions as $afnId) {
+						$this->addAuth($editorId, $afnId, $repId, $uIdToAuths);
+// 					$authZ->createAuthorization(
+// 						$editorId,
+// 						$afnId,
+// 						$repId);
+					}
+				}
+			}
+			if (isset($repAuths['presenters'])) {
+				foreach ($repAuths['presenters'] as $presenterId) {
+					foreach ($viewFunctions as $vfnId) {
+						$this->addAuth($presenterId, $vfnId, $repId, $uIdToAuths);
+// 					$authZ->createAuthorization(
+// 						$presenterId,
+// 						$vfnId,
+// 						$repId);
+					}
+				}
+			}
+			if (isset($repAuths['search'])) {
+				$grpId =& $idManager->getId($repAuths['search']);
+				foreach ($viewFunctions as $vfnId) {
+						$this->addAuth($grpId, $vfnId, $repId, $uIdToAuths);
+// 					$authZ->createAuthorization(
+// 						$grpId,
+// 						$vfnId,
+// 						$repId);
+				}
+			}
+		}
+	// ===== all authorizations for collections should be set
+			
+		foreach ($exhibits as $exAuths) {
+			$exId =& $exAuths['id'];
+			if (isset($exAuths['presenters'])) {
+				foreach ($exAuths['presenters'] as $editorId) {
+					foreach ($viewFunctions as $vfnId) {
+						$this->addAuth($editorId, $vfnId, $exId, $uIdToAuths);
+// 					$authZ->createAuthorization(
+// 						$editorId,
+// 						$vfnId,
+// 						$exId);
+					}
+					foreach ($editFunctions as $efnId) {
+						$this->addAuth($editorId, $efnId, $exId, $uIdToAuths);
+// 					$authZ->createAuthorization(
+// 						$editorId,
+// 						$efnId,
+// 						$exId);
+					}
+				}
+			}
+			if (isset($exAuths['owner'])) {
+				foreach ($viewFunctions as $vfnId) {
+						$this->addAuth($editorId, $vfnId, $exId, $uIdToAuths);
+// 					$authZ->createAuthorization(
+// 						$editorId,
+// 						$vfnId,
+// 						$exId);
+				}
+				foreach ($editFunctions as $efnId) {
+						$this->addAuth($editorId, $efnId, $exId, $uIdToAuths);
+// 					$authZ->createAuthorization(
+// 						$editorId,
+// 						$efnId,
+// 						$exId);
+				}
+				foreach ($adminFunctions as $afnId) {
+						$this->addAuth($editorId, $afnId, $exId, $uIdToAuths);
+// 					$authZ->createAuthorization(
+// 						$editorId,
+// 						$afnId,
+// 						$exId);
+				}
+			}
+		}
+		
+// this is where the magic happens (if you have a lot of old authz's you may want to add statusstars to this loop)
+
+		foreach ($uIdToAuths as $uidstring => $fids) {
+			foreach ($fids as $fidstring => $qids) {
+				foreach ($qids as $qidstring => $true) {
+					$authZ->createAuthorization(
+						$idManager->getId($uidstring),
+						$idManager->getId($fidstring),
+						$idManager->getId($qidstring));
+				}
+			}
+		}
+
+	// ===== all authorizations for exhibitions should be set
+
+		$centerPane->add(new Block(ob_get_contents(), 1));
+		ob_end_clean();
+
+		$dbHandler->disconnect($mdbIndex);
+
+		return true;
+	}
+	
+	function addAuth (&$uid, &$fid, &$qid, &$uIdToAuths) {
+		if (!isset($uIdToAuths[$uid->getIdString()]))
+			$uIdToAuths[$uid->getIdString()] = array();
+		if (!isset($uIdToAuths[$uid->getIdString()][$fid->getIdString()]))
+			$uIdToAuths[$uid->getIdString()][$fid->getIdString()] = array();
+		if (!isset($uIdToAuths[$uid->getIdString()][$fid->getIdString()][$qid->getIdString()]))
+				$uIdToAuths[$uid->getIdString()][$fid->getIdString()][$qid->getIdString()] = true;
+//		}
+	}
+}
+?>
---
+++ main/modules/admin/exportexhibmdb.act.php
@@ -0,0 +1,313 @@
+<?php
+
+require_once(POLYPHONY."/main/library/AbstractActions/MainWindowAction.class.php");
+require_once(POLYPHONY."/main/library/Importer/XMLImporters/XMLImporter.class.php");
+
+
+//apd_set_pprof_trace(); 
+
+class exportexhibmdbAction 
+	extends MainWindowAction {
+	/**
+	 * Check Authorizations
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function isAuthorizedToExecute () {
+		return TRUE;
+	}
+	
+	/**
+	 * Return the heading text for this action, or an empty string.
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function getHeadingText () {
+		return _("Exhibition Export/Import");
+	}
+
+	/**
+	 * Build the content for this action
+	 * 
+	 * @return void
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function buildContent () {	
+		$dbHandler =& Services::getService("DBHandler");
+		$repositoryManager =& Services::getService("Repository");
+		$idManager =& Services::getService("Id");
+		
+print "lines 46 - 48 in exportexhibmdb.act.php need to be modified for database access";
+//		$mdbIndex = $dbHandler->addDatabase(
+//			new MySQLDatabase("host", "db", "uname", "password"));
+//		$dbHandler->connect($mdbIndex);
+exit();
+		$dbHandler->connect(IMPORTER_CONNECTION);
+
+		
+		$exhibitionsQuery =& new SelectQuery;
+		$exhibitionsQuery->addTable("pressets");
+		$exhibitionsQuery->addColumn("id", "exhibitionId");
+		$exhibitionsQuery->addColumn("title", "exhibitionName");
+		$exhibitionsQuery->addColumn("presentations", "slideshowIds");
+		$exhibitionsQuery->addWhere("presentations IS NOT NULL");
+		$exhibitionsQuery->addOrderBy("id");
+		
+		$exhibitionsResults =& $dbHandler->query($exhibitionsQuery, $mdbIndex);
+
+		while ($exhibitionsResults->hasMoreRows()) {
+			$exhibition = $exhibitionsResults->next();
+			
+			$this->openExhibition($exhibition);
+			
+			$slideshowsQuery =& new SelectQuery;
+			$slideshowsQuery->addTable("preslists");
+			$slideshowsQuery->addColumn("id", "slideshowId");
+			$slideshowsQuery->addColumn("title", "slideshowName");
+			$slideshowsQuery->addColumn("description", "slideshowDescription");
+			$slideshowsQuery->addColumn("preslist", "slideOrder");
+			$slideshowsQuery->addWhere("preslist IS NOT NULL");
+			$slideshowsQuery->addWhere("presset = ".
+				$exhibition['exhibitionId']);
+			$slideshowsQuery->addOrderBy("id");
+			
+			$slideshowsResults =& $dbHandler->query($slideshowsQuery,
+				$mdbIndex);
+
+			while ($slideshowsResults->hasNext()) {
+				$slideshow = $slideshowsResults->next();
+				
+				$this->openSlideshow($slideshow);
+				
+				$order = explode(",", $slideshow['slideOrder']);
+				foreach ($order as $presmediaId) {
+					$slideQuery =& new SelectQuery;
+					$slideQuery->addTable("presmedia");
+					$slideQuery->addColumn("comment", "slideCaption");
+					$slideQuery->addColumn("media_id");
+					$slideQuery->addWhere("pres_id = "
+						.$slideshow['slideshowId']);
+					$slideQuery->addWhere("id = ".$presmediaId);
+					
+					$slideResult =& $dbHandler->query($slideQuery, $mdbIndex);
+
+					if ($slideResult->getNumberOfRows() == 1) {
+						$slide = $slideResult->getCurrentRow();
+					
+						$mediaQuery =& new SelectQuery;
+						$mediaQuery->addTable("media");
+						$mediaQuery->addColumn("id");
+						$mediaQuery->addColumn("fname");
+						$mediaQuery->addWhere("id = ".$slide['media_id']);
+						$mediaResult =& $dbHandler->query($mediaQuery, 
+							$mdbIndex);
+						if ($mediaResult->getNumberOfRows() == 1) {
+							$media = $mediaResult->getCurrentRow();
+							
+							$idQuery =& new SelectQuery;
+							$idQuery->addTable("dr_file");
+							$idQuery->addTable("dr_asset_record", INNER_JOIN,
+								"dr_file.id = dr_asset_record.FK_record");
+							$idQuery->addColumn("dr_asset_record.FK_asset",
+								"asset_id");
+							$idQuery->addColumn("dr_file.filename");
+							$idQuery->addWhere("dr_file.filename = '".
+								rawurlencode($media['fname'])."'");
+							
+							$idResult =& $dbHandler->query($idQuery, 
+								IMPORTER_CONNECTION);
+							if ($idResult->getNumberOfRows() == 1) {
+								$idRow = $idResult->getCurrentRow();
+								$this->addSlide($slide, $idRow['asset_id']);
+							}
+// @todo handle multiple id's
+							$idResult->free();
+							unset($idQuery);
+						} else {
+							$empty = "";
+							$this->addSlide($slide, $empty);		
+						}
+						$mediaResult->free();
+						unset($mediaQuery);
+					}
+//					else
+//						print "Bad presmedia: ".$presmediaId."<br />";
+					$slideResult->free();
+					unset($slideQuery);
+				}
+				unset($order);
+				$this->closeSlideshow();
+			}
+			$slideshowsResults->free();
+			unset($slideshowQuery);
+			
+			$this->closeAndImportExhibition();
+		}
+		$exhibitionsResults->free();
+		unset($exhibitionsQuery);		
+
+		$centerPane =& $this->getActionRows();
+		ob_start();
+
+// 		if ($this->_importer->hasErrors()) {
+// 			print("The bad news is that some errors occured during import, they are: <br />");
+// 			$this->_importer->printErrorMessages();
+// 		}
+// 		if ($this->_importer->hasAssets()) {
+// 			print("The good news is that ".count($this->_importer->getGoodAssetIds())." assets were created during import, they are: <br />");
+// 			$this->_importer->printGoodAssetIds();
+// 		}
+		
+		$centerPane->add(new Block(ob_get_contents(), 1));
+		ob_end_clean();
+
+		$dbHandler->disconnect($mdbIndex);
+
+		return true;
+	}
+
+	/**
+	 * Begins a new file for a new Exhibition
+	 *
+	 * @param array $slide
+	 * @since 8/9/05
+	 */
+	function openExhibition (&$exhibition) {
+		$this->_destFolder = 'mdbExhib1_concExhib2';
+		if (!is_dir("/tmp/".$this->_destFolder))
+			mkdir("/tmp/".$this->_destFolder);
+
+		if (!is_dir("/tmp/".$this->_destFolder."/".$exhibition['exhibitionId']))
+			mkdir("/tmp/".$this->_destFolder."/".$exhibition['exhibitionId']);
+		$this->_xmlFile = fopen("/tmp/".$this->_destFolder."/".
+			$exhibition['exhibitionId']."/metadata.xml", "w");
+		fwrite($this->_xmlFile,
+"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+
+		fwrite($this->_xmlFile, "<import>\n");
+		fwrite($this->_xmlFile, "\t<repository 
+			id=\"edu.middlebury.concerto.exhibition_repository\">\n");
+// recordstructure
+		fwrite($this->_xmlFile, "\t\t<recordstructure id=\"Repository::edu.middlebury.concerto.exhibition_repository::edu.middlebury.concerto.slide_record_structure\" xml:id=\"slidestructure\">\n".
+			"\t\t\t<name>Slide Schema</name>\n".
+			"\t\t\t<partstructure id=\"Repository::edu.middlebury.concerto.exhibition_repository::edu.middlebury.concerto.slide_record_structure.edu.middlebury.concerto.slide_record_structure.target_id\" xml:id=\"target id\" isMandatory=\"FALSE\" isRepeatable=\"FALSE\" isPopulated=\"FALSE\">
+				<name>target id</name>
+				<type>
+					<domain>Repository</domain>
+					<authority>edu.middlebury.harmoni</authority>
+					<keyword>string</keyword>
+				</type>
+</partstructure>\n".
+			"\t\t\t<partstructure id=\"Repository::edu.middlebury.concerto.exhibition_repository::edu.middlebury.concerto.slide_record_structure.edu.middlebury.concerto.slide_record_structure.text_position\" xml:id=\"text position\" isMandatory=\"FALSE\" isRepeatable=\"FALSE\" isPopulated=\"FALSE\">
+				<name>text position</name>
+				<type>
+					<domain>Repository</domain>
+					<authority>edu.middlebury.harmoni</authority>
+					<keyword>string</keyword>
+				</type>
+			</partstructure>\n".
+			"\t\t\t<partstructure id=\"Repository::edu.middlebury.concerto.exhibition_repository::edu.middlebury.concerto.slide_record_structure.edu.middlebury.concerto.slide_record_structure.display_metadata\" xml:id=\"display metadata\" isMandatory=\"FALSE\" isRepeatable=\"FALSE\" isPopulated=\"FALSE\">
+				<name>display metadata</name>
+				<type>
+					<domain>Repository</domain>
+					<authority>edu.middlebury.harmoni</authority>
+					<keyword>boolean</keyword>
+				</type>
+			</partstructure>\n".
+		"\t\t</recordstructure>\n");
+// recordstructure
+		fwrite($this->_xmlFile, "\t\t<asset>\n".
+			"\t\t\t<name>".$exhibition['exhibitionName']."</name>\n".
+			"\t\t\t<description></description>\n".
+			"\t\t\t<type>\n".
+			"\t\t\t\t<domain>Asset Types</domain>\n".
+			"\t\t\t\t<authority>edu.middlebury.concerto</authority>\n".
+			"\t\t\t\t<keyword>Exhibition</keyword>\n".
+			"\t\t\t</type>\n");
+	}
+	
+	/**
+	 * Opens a new slideshow
+	 *
+	 * @param array $slide
+	 * @since 8/9/05
+	 */
+	function openSlideshow (&$slideshow) {
+		fwrite($this->_xmlFile, "\t\t\t<asset maintainOrder=\"TRUE\">\n".
+			"\t\t\t\t<name>".$slideshow['slideshowName']."</name>\n".
+			"\t\t\t\t<description><![CDATA[".$slideshow['slideshowDescription'].
+				"]]></description>\n".
+			"\t\t\t\t<type>\n".
+			"\t\t\t\t\t<domain>Asset Types</domain>\n".
+			"\t\t\t\t\t<authority>edu.middlebury.concerto</authority>\n".
+			"\t\t\t\t\t<keyword>Slideshow</keyword>\n".
+			"\t\t\t\t</type>\n");
+	}
+	
+	/**
+	 * Opens a new slideshow
+	 *
+	 * @since 8/9/05
+	 */
+	function closeSlideshow () {
+		fwrite($this->_xmlFile, "\t\t\t</asset>\n");
+	}	
+	
+	/**
+	 * Creates an entire slide entry in the xmlFile
+	 *
+	 * @param array $slide
+	 * @since 8/9/05
+	 */
+	function addSlide (&$slide, &$asset_id) {
+	fwrite($this->_xmlFile, "\t\t\t\t<asset>\n".
+			"\t\t\t\t\t<name></name>\n".
+			"\t\t\t\t\t<description><![CDATA[".
+				$slide['slideCaption']."]]></description>\n".
+			"\t\t\t\t\t<type>\n".
+			"\t\t\t\t\t\t<domain>Asset Types</domain>\n".
+			"\t\t\t\t\t\t<authority>edu.middlebury.concerto</authority>\n".
+			"\t\t\t\t\t\t<keyword>Slide</keyword>\n".
+			"\t\t\t\t\t</type>\n".
+			"\t\t\t\t\t<record xml:id=\"slidestructure\">\n".
+			"\t\t\t\t\t\t<part xml:id=\"target id\">".
+				$asset_id."</part>\n".
+			"\t\t\t\t\t\t<part xml:id=\"text position\">right</part>\n".
+			"\t\t\t\t\t\t<part xml:id=\"display metadata\">false</part>\n".
+			"\t\t\t\t\t</record>\n".
+			"\t\t\t\t</asset>\n");
+	}
+
+	/**
+	 * Closes and Imports a single exhibition
+	 *
+	 * @since 8/9/05
+	 */
+	function closeAndImportExhibition () {
+		fwrite($this->_xmlFile,	"\t\t</asset>\n".
+			"\t</repository>\n".
+			"</import>");
+		fclose($this->_xmlFile);
+	
+		$array = array("edu.middlebury.concerto.exhibition_repository",
+"Repository::edu.middlebury.concerto.exhibition_repository::edu.middlebury.concerto.slide_record_structure",
+"Repository::edu.middlebury.concerto.exhibition_repository::edu.middlebury.concerto.slide_record_structure.edu.middlebury.concerto.slide_record_structure.target_id",
+"Repository::edu.middlebury.concerto.exhibition_repository::edu.middlebury.concerto.slide_record_structure.edu.middlebury.concerto.slide_record_structure.text_position",
+"Repository::edu.middlebury.concerto.exhibition_repository::edu.middlebury.concerto.slide_record_structure.edu.middlebury.concerto.slide_record_structure.display_metadata");
+
+// 		if (isset($this->_importer))
+// 			unset($this->_importer);
+// 		
+// 		$this->_importer =& XMLImporter::withFile($array, 
+// 			"/home/cshubert/public_html/importer/importtest/metadata.xml",
+// 			"insert");
+// 
+// 		$this->_importer->parseAndImportBelow();
+	}
+}
+?>
