69fa584fdcf359fa1dfbbf0e6fb9109a9c2ac5de
Tim Bahls <tbahls@middlebury.edu>
2006-06-27 19:28:24 +0000

    Configuration for coursemanagement

--- config/coursemanagement_default.conf.php
+++ config/coursemanagement_default.conf.php
@@ -1,22 +1,30 @@
 <?php
 
 /**
- * Set up the CourseManagementManager
- *
- * USAGE: Copy this file to coursemanagament.conf.php to set custom values.
- *
- * @package concerto.config
- * 
- * @copyright Copyright &copy; 2006, Middlebury College
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
- *
- * @version $Id$
- */
- 
+* Set up the CourseManagementManager
+*
+* USAGE: Copy this file to coursemanagament.conf.php to set custom values.
+*
+* @package concerto.config
+*
+* @copyright Copyright &copy; 2006, Middlebury College
+* @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+*
+* @version $Id$
+*/
+
 // :: Set up the CourseManagementManager ::
-	$courseManagamentHierarchyId = "edu.middlebury.authorization.hierarchy";
-	$configuration =& new ConfigurationProperties;
-	$configuration->addProperty('database_index', $dbID);
-	$configuration->addProperty('hierarchy_id', $courseManagamentHierarchyId);
-	Services::startManagerAsService("CourseManagementManager", $context, $configuration);
-	
+$configuration =& new ConfigurationProperties;
+$configuration->addProperty('database_index', $dbID);
+
+$courseManagamentHierarchyId = "edu.middlebury.authorization.hierarchy";
+$courseManagementId ="edu.middlebury.coursemanagement";
+$canonicalCoursesId="edu.middlebury.coursemanagement.canonicalcourses";
+$courseGroupsId ="edu.middlebury.coursemanagement.coursegroups";
+
+$configuration->addProperty('hierarchy_id', $courseManagamentHierarchyId);
+$configuration->addProperty('course_management_id', $courseManagementId);
+$configuration->addProperty('canonical_courses_id', $canonicalCoursesId);
+$configuration->addProperty('course_groups_id', $courseGroupsId);
+
+Services::startManagerAsService("CourseManagementManager", $context, $configuration);
