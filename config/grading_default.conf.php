c73e5d5a91d2ffb31966a41681b66522bb54bd31
Tim Bahls <tbahls@middlebury.edu>
2006-07-18 15:03:21 +0000

    configuring the grading manager

---
+++ config/grading_default.conf.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* Set up the GradingManager
+*
+* USAGE: Copy this file to grading.conf.php to set custom values.
+*
+* @package concerto.config
+*
+* @copyright Copyright &copy; 2006, Middlebury College
+* @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+*
+* @version $Id$
+*/
+
+// :: Set up the CourseManagementManager ::
+$configuration =& new ConfigurationProperties;
+
+Services::startManagerAsService("GradingManager", $context, $configuration);
