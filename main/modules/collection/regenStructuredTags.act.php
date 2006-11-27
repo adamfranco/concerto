b4387ed184bc5a235e3eb0c994c807b3ef5944c4
Adam Franco <afranco@middlebury.edu>
2006-11-27 22:41:13 +0000

    Adding methods to support auto-generation of tags from structured metadata.

---
+++ main/modules/collection/regenStructuredTags.act.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @package concerto.modules.collection
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id$
+ */
+require_once(POLYPHONY."/main/library/AbstractActions/MainWindowAction.class.php");
+
+/**
+ * 
+ * 
+ * @package concerto.modules.collection
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id$
+ */
+class regenStructuredTagsAction 
+	extends MainWindowAction
+{
+	/**
+	 * Check Authorizations
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function isAuthorizedToExecute () {
+		// Check that the user can delete this exhibition
+		$authZ =& Services::getService("AuthZ");
+		$idManager =& Services::getService("Id");
+		return $authZ->isUserAuthorized(
+					$idManager->getId("edu.middlebury.authorization.modify"), 
+					$idManager->getId(RequestContext::value('collection_id')));
+	}
+
+	/**
+	 * Return the "unauthorized" string to pring
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function getUnauthorizedMessage () {
+		return _("You are not authorized to regenerate stuctured tags for this <em>Collection</em> or its <em>Assets</em>.");
+	}
+
+	/**
+	 * Return the heading text for this action, or an empty string.
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function getHeadingText () {
+		$idManager =& Services::getService("Id");
+		$repositoryManager =& Services::getService("Repository");
+		$repository =& $repositoryManager->getRepository(
+				$idManager->getId(RequestContext::value('collection_id')));
+		return _("Regenerate Structured Tags for Collection")." <em>".$repository->getDisplayName().
+			"</em> ";
+	}
+
+	/**
+	 * Build the content for this action
+	 * 
+	 * @return void
+	 * @access public
+	 * @since 4/26/05
+	 */
+	function buildContent () {
+		$actionRows =& $this->getActionRows();
+		$harmoni =& Harmoni::instance();
+		
+		$idManager =& Services::getService("Id");
+		$repositoryId =& $idManager->getId(RequestContext::value('collection_id'));
+		
+
+		$systemAgentId =& $idManager->getId('system:concerto');
+		$tagGenerator =& StructuredMetaDataTagGenerator::instance();	
+		$tagGenerator->regenerateTagsForRepository($repositoryId, $systemAgentId,
+			'concerto');
+		
+
+		// Log the success or failure
+		if (Services::serviceRunning("Logging")) {
+			$loggingManager =& Services::getService("Logging");
+			$log =& $loggingManager->getLogForWriting("Concerto");
+			$formatType =& new Type("logging", "edu.middlebury", "AgentsAndNodes",
+							"A format in which the acting Agent[s] and the target nodes affected are specified.");
+			$priorityType =& new Type("logging", "edu.middlebury", "Event_Notice",
+							"Normal events.");
+			
+			$item =& new AgentNodeEntryItem("Regenerated Tags", "Auto-generated tags were regenerated");
+			$item->addNodeId($repositoryId);
+			
+			$log->appendLogWithTypes($item,	$formatType, $priorityType);
+		}
+		
+		RequestContext::sendTo(
+			$harmoni->request->quickURL("collections", "namebrowse"));
+	}
+}
