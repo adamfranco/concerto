58c67a994691f111bd2d180edb0a372e8b538299
Adam Franco <afranco@middlebury.edu>
2007-04-10 18:33:55 +0000

    Fixing bad default preference that became apparent when testing in PHP 5.

--- config/repository_default.conf.php
+++ config/repository_default.conf.php
@@ -21,7 +21,7 @@
 	$configuration->addProperty('hierarchy_id', $repositoryHierarchyId);
 	$configuration->addProperty('default_parent_id', $arg1 = REPOSITORY_ROOT_ID);
 	$configuration->addProperty('version_control_all', $arg2 = TRUE);
-	$configuration->addProperty('use_filesystem_for_files', $arg3 = TRUE);
+	$configuration->addProperty('use_filesystem_for_files', $arg3 = FALSE);
 // 	$configuration->addProperty('file_data_path', $arg4 = MYPATH."/../concerto_data");
 	Services::startManagerAsService("RepositoryManager", $context, $configuration);
 	unset($arg0, $arg1, $arg2, $arg3, $arg4);
