d63651b885944ac7ca00cbef91472f7c3baca8d4
Adam Franco <afranco@middlebury.edu>
2008-03-12 21:04:34 +0000

    Adding export config.

---
+++ config/export_default.conf.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @since 1/28/08
+ * @package segue.config
+ * 
+ * @copyright Copyright &copy; 2008, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id$
+ */ 
+ 
+if (!defined('XML_EXPORT_TMP_DIR'))
+	define('XML_EXPORT_TMP_DIR', '/tmp');
